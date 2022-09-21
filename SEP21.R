328770d5ce6272fd25bdaac89e036f3f80d54a69
tboone14 <98772089+tboone14@users.noreply.github.com>
2022-09-21 15:25:52 -0400

    Add files via upload

---
+++ SEP21.R
@@ -0,0 +1,7 @@
+#Author: Boone Jr, Date: 09/21/2022, Purpose: Perform ANOVA analysis on sample dataset
+
+PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv" 
+
+df <- read.csv(PATH)
+
+df
